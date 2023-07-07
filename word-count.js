755ac9eef146a3791b302420e9f23fa2fa30500b
Dayanand <angularexpertdayanand@gmail.com>
2023-07-07 12:57:36 +0530

    Javascript file

---
+++ word-count.js
@@ -0,0 +1,15 @@
+function wordCount(word){
+    return word.split(" ").length;
+}
+
+let statement = "Hello Dayanand Kumar Kasagala";
+
+console.log(wordCount(this.wordCount))
+
+// function countWords(s){
+//     s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
+//     s = s.replace(/[ ]{2,}/gi," ");//2 or more space to 1
+//     s = s.replace(/\n /,"\n"); // exclude newline with a start spacing
+//     return s.split(' ').filter(function(str){return str!="";}).length;
+//     //return s.split(' ').filter(String).length; - this can also be used
+// }
