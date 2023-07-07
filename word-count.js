3964535f74da4f111e07971353c38e7a5411b677
Dayanand <angularexpertdayanand@gmail.com>
2023-07-07 13:09:59 +0530

    code has been written

--- word-count.js
+++ word-count.js
@@ -1,11 +1,18 @@
+//way 1
 function getCount(str) {
-    return str.split(' ').filter(function(num) {
-     return num != ''
+    return str.split(' ').filter(function (num) {
+        return num != ''
     }).length;
-   }
+}
 
-   var string = 'LinuxHint is the best Website for learning skills';
-   console.log('Total Words in String: '+ getCount(string));
+//way2
+function getCount(str) {
+    return str.split(' ').length
+}
+var string = 'LinuxHint is the best Website for learning skills';
+console.log('Total Words in String: ' + getCount(string));
+
+
 
 // function countWords(s){
 //     s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
